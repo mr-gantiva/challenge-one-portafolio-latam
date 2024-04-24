0e41763273573aabe57d5f164131b02c817fa464
mr-gantiva <ivaganva@gmail.com>
2024-04-23 22:34:34 -0400

    Add animations

---
+++ assets/js/efectos.js
@@ -0,0 +1,70 @@
+// gsap.from(".skills__box", {
+//   duration: 1,
+//   x: 150,
+//   delay: 1,
+//   ease: "bounce.out",
+// });
+
+// gsap.registerPlugin(ScrollTrigger);
+
+// let tl = gsap.timeline({
+//   repeat: 0,
+// });
+
+// tl.from(".skill-1", {
+//   ScrollTrigger: ".hobbies-1",
+//   duration: 0.5,
+//   y: 200,
+// });
+// tl.from(".skill-2", {
+//   duration: 0.5,
+//   y: 200,
+// });
+
+// tl.from(".skill-3", {
+//   duration: 0.5,
+//   y: 200,
+// });
+
+// tl.from(".skill-4", {
+//   duration: 0.5,
+//   y: 200,
+// });
+
+// tl.from(".skill-5", {
+//   duration: 0.5,
+//   y: 200,
+// });
+// tl.from(".skill-6", {
+//   duration: 0.5,
+//   y: 200,
+// });
+
+// tl.from(".hobbies-1", {
+//   ScrollTrigger: ".skill-1",
+//   duration: 0.5,
+//   y: 200,
+// });
+// tl.from(".hobbies-2", {
+//   duration: 0.5,
+//   y: 200,
+// });
+
+// tl.from(".hobbies-3", {
+//   duration: 0.5,
+//   y: 200,
+// });
+
+// tl.from(".hobbies-4", {
+//   duration: 0.5,
+//   y: 200,
+// });
+
+// tl.from(".hobbies-5", {
+//   duration: 0.5,
+//   y: 200,
+// });
+// tl.from(".hobbies-6", {
+//   duration: 0.5,
+//   y: 200,
+// });
